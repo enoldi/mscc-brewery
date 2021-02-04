43f17329c49bc7bb866ef365be2d3942267accd6
arsene.chamogue <arsene.chamgoue@sprint-pay.com>
2021-02-04 08:15:31 +0100

    add v1

--- src/main/java/com/prim4s/msccbrewery/web/services/impl/BeerServiceImpl.java
+++ src/main/java/com/prim4s/msccbrewery/web/services/impl/BeerServiceImpl.java
@@ -32,6 +32,7 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
         // todo to impl methode to update beer
+        log.debug("Updating beerDto : {}", beerDto);
     }
 
     @Override
